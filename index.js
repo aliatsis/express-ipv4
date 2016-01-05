e1116fdb561476b09e779876f24b1709a6d33743
Anthony Liatsis <anthony.liatsis@gmail.com>
2016-01-05 01:13:20 -0800

    return middleware

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var ipaddr = require('ipaddr.js');
 
-module.exports = function(req, res, next) {
-  var ip = req.ip;
+module.exports = function() {
+  return function(req, res, next) {
+    var ip = req.ip;
 
-  Object.defineProperty(req, 'ip', {
-    get: function() {
-      return ip;
-    },
-    set: function(val) {
-      ip = ipaddr.process(val).toString();
-    }
-  });
+    Object.defineProperty(req, 'ip', {
+      get: function() {
+        return ip;
+      },
+      set: function(val) {
+        ip = ipaddr.process(val).toString();
+      }
+    });
 
-  req.ip = req.ip;
-  next();
-};
+    req.ip = req.ip;
+    next();
+  };
+}
